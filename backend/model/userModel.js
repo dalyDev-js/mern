7cdc8f3a6744c97c4f30d14e0195688d8986a715
ahmed hassan <ahmedhsn787@gmail.com>
2024-08-30 06:52:41 +0300

    landing page

---
+++ backend/model/userModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["engineer", "client"],
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
