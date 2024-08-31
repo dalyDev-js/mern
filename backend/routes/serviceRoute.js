d6fbeda557e463e89fc7fe1bbb8885dd3db480ed
ahmed hassan <ahmedhsn787@gmail.com>
2024-08-31 21:24:11 +0300

    glopal handling error & service Routes $ controller

--- backend/routes/serviceRoute.js
+++ backend/routes/serviceRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getAllServices } from "../controllers/serviceController.js";
+import {
+  createService,
+  deleteService,
+  getAllServices,
+  getService,
+  updateService,
+} from "../controllers/serviceController.js";
 const router = express.Router();
 
-router.route("/").get(getAllServices);
-// .post(createService);
-// router.route("/:id").get(getService).patch(updateService).delete(deleteService);
+router.route("/").get(getAllServices).post(createService);
+router.route("/:id").get(getService).patch(updateService).delete(deleteService);
 
 export default router;
