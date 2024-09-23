997af6891c3796beba301472f01eae83fdf7022f
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-09-23 17:45:30 +0300

    backedn endpoints

--- backend/routes/verify.routes.js
+++ backend/routes/verify.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { updateVerifiedStatus } from "../controllers/verify.controller.js";
+import { getUsersPendingVerification } from "../controllers/userController.js";
 
 const verifyRouter = express.Router();
 
 verifyRouter.post("/user/:id", updateVerifiedStatus);
-
+verifyRouter.get("/pendingVerify", getUsersPendingVerification);
 export default verifyRouter;
