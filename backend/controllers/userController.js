1b045a3b1105ab601c67f69053e785a25c7b6060
Abdulrhman El-Daly <abd911daly@gmail.com>
2024-09-25 01:23:55 +0300

    client by id function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import catchAsync from "../utils/catchAsync.js";
 // userController.js
 
 // Fetch user by ID
+
 export const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -112,6 +113,61 @@ export const getUsersForSidebar = catchAsync(async (req, res, next) => {
     status: "success",
     data: {
       filteredusers,
+    },
+  });
+});
+
+export const deleteUserById = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const user = await User.findByIdAndDelete(id);
+
+  if (!user) {
+    return next(new AppError("No user found with that ID", 404));
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+export const updateUserById = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  // Check if user is trying to update the password using this route
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for updating password. Please use /updatePassword.",
+        400
+      )
+    );
+  }
+
+  // Filter out fields that are not allowed to be updated
+  const filteredBody = filterObj(
+    req.body,
+    "fullName",
+    "username",
+    "email",
+    "gender",
+    "profilePic"
+  );
+
+  // Update user data
+  const updatedUser = await User.findByIdAndUpdate(id, filteredBody, {
+    new: true, // Returns the updated user
+    runValidators: true, // Runs validation on the updated fields
+  });
+
+  if (!updatedUser) {
+    return next(new AppError("No user found with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
     },
   });
 });
--- backend/routes/userRouts.js
+++ backend/routes/userRouts.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
   deleteMe,
+  deleteUserById,
   getUserById,
   getUsersPendingVerification,
   updateMe,
+  updateUserById,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/protectRoute.js";
 
@@ -13,5 +15,8 @@ userRouter.get("/getUser/:id", getUserById);
 userRouter.patch("/updateMe", protect, updateMe);
 userRouter.delete("/deleteMe", deleteMe);
 userRouter.get("/pending-verification", getUsersPendingVerification);
+
+userRouter.patch("/updateUser/:id", updateUserById);
+userRouter.delete("/deleteUser/:id", deleteUserById);
 
 export default userRouter;
